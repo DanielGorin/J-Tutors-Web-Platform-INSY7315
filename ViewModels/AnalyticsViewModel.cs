b3d82889c260a6399c01213f11d954fcfb026da2
William Mcpetrie <131513420+gatekeeper321@users.noreply.github.com>
2025-10-31 22:10:41 +0200

    added analytics

---
+++ ViewModels/AnalyticsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using J_Tutors_Web_Platform.Models;
+
+namespace J_Tutors_Web_Platform.ViewModels
+{
+    public class AnalyticsViewModel
+    {
+        public int GlobalPoints { get; set; }
+        public double AverageAttendance { get; set; }
+        public double AmountUnpaid { get; set; }
+        public double MonthlyEarnings { get; set; }
+
+    }
+}
